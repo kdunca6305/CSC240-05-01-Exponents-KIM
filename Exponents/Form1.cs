0208f61ec36df1c6693bb8b460f72aaa51185755
kdunca6305 <82078884+kdunca6305@users.noreply.github.com>
2022-02-07 13:38:55 -0500

    Update Form1.cs

--- Exponents/Form1.cs
+++ Exponents/Form1.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 
 /**************************************
- * Name:
- * Date:
+ * Name: Kayla Mead
+ * Date: February 7th, 2022
  * Description: A program using user-created method
  *              for the math function Square and Cube
  * Assistance:
